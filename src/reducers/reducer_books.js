02b83033e3905ea88428a5ffdd5bbf7a30624c5b
Joe Schafer <joeschaferastula@gmail.com>
2018-06-11 10:19:45 -0500

    added book reducer

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+export default function() {
+  return [
+    { title: 'The Temple of the Golden Pavillion'},
+    { title: 'The Greeks'},
+    { title: 'Buddha\'s Brain'},
+    { title: 'Mere Christianity'},
+    { title: 'Sapiens'}
+  ]
+}
